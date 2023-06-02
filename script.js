7630084678d794c3a5f338ae3156bc0502f6223b
orewan <42575815+orewan@users.noreply.github.com>
2023-06-02 13:49:00 +0300

    Update about Tic Tac Toe
    Update about Tic Tac Toe  - Needs to be completed

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
+
+// Open and close the Form where the player choose
 function openForm() {
     document.getElementById("popupForm").style.display = "block";
   }
 
-  function openForm() {
+  function closeForm() {
     document.getElementById("popupForm").style.display = "none";
   }
 
 
+
+// Set the player and the Computer
+
+document.getElementById('popupForm').addEventListener('submit',function(event){
+  event.preventDefault();
+});
 
 
 
@@ -29,23 +37,161 @@ document.getElementById('submit').onclick = function(event) {
 }
 
 
+// Set the choice  of the player and close tthe form then open the board game
 function startTheGame(choiceOfTheplayer,choiceOfTheComputer){
   if(choiceOfTheplayer === "X" && choiceOfTheComputer === "Easy"){
     alert("The player choose X and Computer: Easy");
     console.log("The player choose X and Computer: Easy")
+    closeForm();
+  //  displayBoard();
+    createBoard();
   }
   if(choiceOfTheplayer === "O" && choiceOfTheComputer === "Easy"){
     alert("The player choose O and Computer: Easy");
+    closeForm();
+  //  displayBoard();
+    createBoard();
   }
   
   if(choiceOfTheplayer === "X" && choiceOfTheComputer === "Hard"){
     alert("The player choose X and Computer: Hard");
+    closeForm();
+  //  displayBoard();
+    createBoard();
   }
   if(choiceOfTheplayer === "O" && choiceOfTheComputer === "Hard"){
     alert("The player choose O and Computer: Hard");
+    closeForm();
+  //  displayBoard();
+    createBoard();
   }
   
 }
+
+
+// Function Board Game and Tic Tac Toe Information about the player
+const gameBoard = document.querySelector("#gameBoard");
+const infoDisplay = document.querySelector("#infoOfTheplayer");
+
+
+
+// function to Display Board
+/*
+function displayBoard(){
+  document.getElementById("gameBoard").style.display = "block";
+}
+*/
+
+// Cells (Empty Array - 9 cases)
+
+const arrayCells = ["","","","","","","","",""];
+
+// Variable of the Circle And variable of the Cross
+
+let go = "circle";
+infoDisplay.textContent = "Circle goes first";
+
+
+
+function createBoard(){
+  
+ 
+  
+  arrayCells.forEach((theCell,index) => {
+     
+      const cellElement = document.createElement('div');
+      cellElement.classList.add('square');
+
+      // Add the index
+      cellElement.id = index
+      // Add the "O" and "X"
+      cellElement.addEventListener('click', addTheSign)
+
+      gameBoard.append(cellElement);
+      
+  })
+
+ 
+}
+
+
+function addTheSign(e){
+  const signDisplay = document.createElement('div');
+  signDisplay.classList.add(go);
+  e.target.append(signDisplay);
+  
+ 
+  go = go === "circle" ? "cross" : "circle"
+  infoDisplay.textContent = "it's turn of " + go; 
+  
+  e.target.removeEventListener('click', addTheSign);
+  
+  checkTheWinner();
+  
+  //Circle then Cross
+  /* Loop 
+  if(go = go === "circle"){
+    go = go === "cross";
+    infoDisplay.textContent = "it's turn of " + go;
+    e.target.removeEventListener('click', addTheSign);
+    
+  }
+  else{
+    go = go === "circle" ;
+    infoDisplay.textContent = "it's turn of " + go; 
+    e.target.removeEventListener('click', addTheSign);
+    
+  }
+  */
+  
+}
+
+
+function checkTheWinner(){
+
+  const allSquares = document.querySelectorAll('.square');
+
+
+  const winningCombos = [
+    [0,1,2],[3,4,5],[6,7,8],
+    [0,3,6],[1,4,7],[2,5,8],
+    [0,4,8],[2,4,6]
+  ]
+
+
+  winningCombos.forEach( array =>{
+    
+    const circleWins = array.every(cell =>
+      allSquares[cell].firstChild?.classList.contains('circle'))
+
+    if(circleWins){
+      infoDisplay.textContent = " Player with Circle wins, Congratulations ! ";
+      allSquares.forEach(square => square.replaceWith(square.cloneNode(true)));
+      return    
+    }
+  
+})
+   
+  winningCombos.forEach( array =>{
+    
+    const crossWins = array.every(cell =>
+    allSquares[cell].firstChild?.classList.contains('cross'))
+
+    if(crossWins){
+      infoDisplay.textContent = " Player with Cross wins, Congratulations ! ";
+      allSquares.forEach(square => square.replaceWith(square.cloneNode(true)));
+      return
+  }
+  
+})
+
+
+
+}
+
+
+
+
 
 
 
